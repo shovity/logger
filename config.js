ce95408f62b930e8eaab5a87670ddba13851d2f3
shovity <shovity@gmail.com>
2019-10-27 23:41:21 +0700

    add post log

--- config.js
+++ config.js
@@ -2,4 +2,9 @@ const config = {}
 
 config.port = 3000
 
+config.redis  = {
+    host: 'redis',
+    port: 6379,
+}
+
 module.exports = config
--- router/index.js
+++ router/index.js
@@ -1,11 +1,39 @@
 'use strict'
 
 const express = require('express')
+const uuid = require('uuid/v4')
+const moment = require('moment')
+
+const redis = require('../redis')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json({ name: 'You know for logging' })
+    return res.json({ name: 'You know for logging' })
+})
+
+
+router.get('/api/v1/log', (req, res) => {
+    
+    return res.json({ a: uuid(), b: moment().format('DD/MM/YYYY')})
+})
+
+router.post('/api/v1/log', (req, res) => {
+
+    const { key, message, data } = req.body
+
+    const id = uuid()
+    const created = Date.now()
+    
+    const log = { id, key, message, data, created }
+
+    redis.zadd(`logger:log:${key}:${moment().format('DD/MM/YYYY')}:${id}`, created, JSON.stringify(log))
+    
+    return res.json({ id })
+})
+
+router.use((req, res, next) => {
+    return res.json({ error: 404 })
 })
 
 module.exports = router
